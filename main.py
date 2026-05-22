b8d6addf1d419075aa6934f341aad852a0719521
Danil Rozhin <144659954+DanilRozhin@users.noreply.github.com>
2026-05-22 15:04:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_login():
     return LOGIN
 
 @app.get("/id/{N}")
-async def get_user_login(N: int):
+async def get_user_login(N: str):
     url = f"https://nd.kodaktor.ru/users/{N}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url, headers={"Content-Type": None})
